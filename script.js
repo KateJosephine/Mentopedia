0d6c5a9117485791dd95a725021bc9a7b2ac8cf5
KateJosephine <113868156+KateJosephine@users.noreply.github.com>
2023-11-12 15:55:05 +0800

    Update script.js

--- script.js
+++ script.js
@@ -26,3 +26,31 @@ window.onscroll  = () => {
         body.classList.remove('active');
     }
 }
+
+let toggleBtn = document.querySelector('#toggle-btn');
+let darkMode = localStorage.getItem('dark-mode');
+
+const enableDarkMode = () => {
+    toggleBtn.classList.replace('fa-moon', 'fa-sun');
+    body.classList.add('dark');
+    localStorage.setItem('dark-mode', 'enabled');
+}
+
+const disableDarkMode = () => {
+    toggleBtn.classList.replace('fa-sun','fa-moon');
+    body.classList.remove('dark');
+    localStorage.setItem('dark-mode', 'disabled');
+}
+
+if (darkMode === 'enabled') {
+    enableDarkMode();
+}
+
+toggleBtn.onclick = (e) => {
+    let darkMode = localStorage.getItem('dark-mode');
+    if (darkMode === 'disabled') {
+        enableDarkMode();
+    } else {
+        disableDarkMode();
+    }
+}
